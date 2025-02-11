97d1912e4fd81e553fc6fa9d679939ba4bdd0dd7
tjm2015 <tim.mcmahon2015@gmail.com>
2025-02-10 20:48:12 -0500

    Added initial player to screen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -9,6 +9,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     while True:
         # Check if user has closed the window and if so exit the fame loop - make's the window close button work
         for event in pygame.event.get():
@@ -16,8 +21,12 @@ def main():
                 return
 
         screen.fill(color="black")
+
+        player.draw(screen)
         
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
